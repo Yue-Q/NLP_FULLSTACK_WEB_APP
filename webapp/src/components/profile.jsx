662bce281f60f011889394e6f990e59c4f214319
Esther Qiu <qiu00057@umn.edu>
2021-01-04 23:05:32 -0600

    profile UI layout

--- webapp/src/components/profile.jsx
+++ webapp/src/components/profile.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Button, Link } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { 
+  Button,
+  Container,
+  Table,
+  Grid,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@material-ui/core';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { APIRequest } from '../utils';
-
-// const SERVER_URL = "http://localhost:5000";
+import axios from 'axios';
+import Auth from '../modules/Auth.js';
 
 const StyledButton = withStyles((theme) => ({
   root: {
@@ -39,37 +50,75 @@ const useStyles = makeStyles((theme) => ({
           backgroundColor: '#ef3e3d'
       }
     }
-  },
-  button: {
-    root:{
-      "&hover": {
-        opacity: 0.3,
-          backgroundColor: '#ef3e3d'
-      }
-    }
-  }
-  
+  }  
 }));
 
-
-
+const CustomizedTableRow = (props) => {
+  return(
+    <TableRow>
+      <TableCell component="th" scope="row">
+        {props.title}:
+      </TableCell>
+      <TableCell>{props.value}</TableCell>
+    </TableRow>
+  )
+}
 
 export default function Profile(props) {  
   const classes = useStyles();
+  // const [user, setUser] = useState(null);
+ 
+  // useEffect(async () => {
+  //   const res = await APIRequest({
+  //     url: `/user/fetchUserInfo`,
+  //     method: "GET",
+  //     token: Auth.getToken()
+  //   });
+  //   const data = await res.json();
+  //   setUser(data.user);
+  // },[]);
+  const [user, setUser] = useState({
+    userName:"test1",
+    firstName:"Yue",
+    middleName:"M.",
+    lastName:"Qiu",
+    occupation:"Student",
+    email:"test@gamil.com",
+    mailAddress:"995 Jefferson Commons Circle"
+  });
 
-  async function handleSignout() {
-    await APIRequest({
-        url: `/user/logout`,
-        method: "GET"
-    })
-  }
 
   return (
-    <div style={{display:"flex",flexDirection:'row', justifyContent:'space-around'}}>
-        <StyledButton>
-            <Link href="/profile">Profile</Link>
-        </StyledButton>
+    <Container maxWidth="xs" className={classes.paper}>
+      <h1>
+        User Information
+      </h1>
+      <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell>Info</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <CustomizedTableRow title="Username" value={user.userName} />
+          <CustomizedTableRow title="Full Name" value={`${user.firstName} ${user.middleName} ${user.lastName}`} />
+          <CustomizedTableRow title="Occupation" value={user.occupation} />
+          <CustomizedTableRow title="Email" value={user.email} />
+          <CustomizedTableRow title="Mail Address" value={user.mailAddress} />
+        </TableBody>
+      </Table>
+      </TableContainer>
+      <Grid container spacing={2} style={{marginTop: "10px"}}>
+          <Grid item xs={12} sm={6}>
+            <Button variant="contained" color="primary" onClick={() => props.history.push('/resetPassword')} fullWidth>Reset Password</Button>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Button variant="contained" color="secondary" onClick={() => {Auth.signout();props.history.replace('/')}} fullWidth>Sign Out</Button>
+          </Grid>
 
-    </div>
+      </Grid>
+    </Container>
   );
   }
